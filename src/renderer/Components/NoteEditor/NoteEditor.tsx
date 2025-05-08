a484b45cf00ae3a7437c5f20b5e0edfbad379369
outlogic <gameundwatch@gmail.com>
2025-05-08 14:03:19 +0900

    NoteToolBar分離

--- src/renderer/Components/NoteEditor/NoteEditor.tsx
+++ src/renderer/Components/NoteEditor/NoteEditor.tsx
@@ -22,6 +22,7 @@ import queryString from 'query-string';
 import './index.css';
 import './MdViewer.css';
 import { Note } from '../../../common/note';
+import { NoteToolbar } from './NoteToolBar';
 
 const closeNote = async (noteId: string) => {
   await window.noteAPI.closeNoteWindow(noteId);
@@ -114,116 +115,7 @@ export function NoteEditor() {
             </Box>
           </Flex>
         </Box>
-
-        <Box width="100%" className="Edit" px="2" py="1" flexGrow="0">
-          <Flex width="100%" direction="row" align="center" justify="between">
-            <Box flexGrow="1">
-              <Flex direction="row" align="center" justify="start" gap="2">
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <ChevronLeftIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <ChevronRightIcon />
-                </IconButton>
-                <Separator orientation="vertical" size="1" />
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <FontBoldIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <FontItalicIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <StrikethroughIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <HeadingIcon />
-                </IconButton>
-                <Separator orientation="vertical" size="1" />
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <CodeIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <QuoteIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <ListBulletIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <CheckboxIcon />
-                </IconButton>
-                <IconButton
-                  type="button"
-                  variant="ghost"
-                  radius="small"
-                  size="1"
-                >
-                  <DotsHorizontalIcon />
-                </IconButton>
-              </Flex>
-            </Box>
-            <IconButton
-              type="button"
-              variant="ghost"
-              radius="none"
-              color="red"
-              size="1"
-              onClick={() => closeNote(note.id)}
-            >
-              <Cross1Icon />
-            </IconButton>
-          </Flex>
-        </Box>
-
+        <NoteToolbar closeNote={() => closeNote(note.id)}/>
         {isActive ? (
           <Box width="100%" flexGrow="1" className="Body">
             <textarea
--- src/renderer/Components/NoteEditor/ToolBar.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from 'react';
-
-export function ToolBar() {
-  return (
-
-  )
-}
